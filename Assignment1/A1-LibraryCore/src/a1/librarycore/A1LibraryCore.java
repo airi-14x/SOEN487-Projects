4410735b3e6de13f65e787d34549ef59da0d1010
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2020-01-28 17:39:45 -0500

    Added projects for all layers

---
+++ Assignment1/A1-LibraryCore/src/a1/librarycore/A1LibraryCore.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a1.librarycore;
+
+/**
+ *
+ * @author jasminelatendresse
+ */
+public class A1LibraryCore {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
