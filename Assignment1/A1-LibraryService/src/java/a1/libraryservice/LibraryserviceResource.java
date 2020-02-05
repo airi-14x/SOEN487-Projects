58b3bfc61f0e32edfd0c0d13760779f123838ea2
airi-14x <airi.14x@gmail.com>
2020-02-05 11:01:56 -0500

    Add back LibraryService

---
+++ Assignment1/A1-LibraryService/src/java/a1/libraryservice/LibraryserviceResource.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a1.libraryservice;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author Airi
+ */
+@Path("LibraryServiceResource")
+public class LibraryserviceResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of LibraryserviceResource
+     */
+    public LibraryserviceResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of a1.libraryservice.LibraryserviceResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getText() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of LibraryserviceResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.TEXT_PLAIN)
+    public void putText(String content) {
+    }
+}
