25dd0332f233b9b4f671c778c36ff4eb77996d58
airi-14x <airi.14x@gmail.com>
2020-02-09 14:50:02 -0500

    Query Param bug fix

--- Assignment1/A1-LibraryClient/src/java/libraryclient/LibraryClient.java
+++ Assignment1/A1-LibraryClient/src/java/libraryclient/LibraryClient.java
@@ -42,6 +42,7 @@ public class LibraryClient {
 
     public Response deleteBook(int id) throws ClientErrorException {
         //webTarget.queryParam("id", id);
+        webTarget = client.target(BASE_URI).path("LibraryRESTService");
         return webTarget.queryParam("id", id).path("book/delete").request().delete(Response.class);
     }
 
@@ -64,6 +65,7 @@ public class LibraryClient {
         //webTarget.queryParam("isbn", isbn);
         //webTarget.queryParam("author", author);
         //webTarget.queryParam("publisher", publisher);
+        webTarget = client.target(BASE_URI).path("LibraryRESTService");
         Entity<?> empty = Entity.text("");
         webTarget = webTarget.queryParam("id", id).queryParam("title", title).queryParam("description", description).queryParam("isbn", isbn).queryParam("author", author).queryParam("publisher", publisher);
         return webTarget.path("book/update").request().put(empty, Response.class);
@@ -81,6 +83,7 @@ public class LibraryClient {
         //webTarget.queryParam("isbn", isbn);
         //webTarget.queryParam("author", author);
         //webTarget.queryParam("publisher", publisher);
+        webTarget = client.target(BASE_URI).path("LibraryRESTService");
         return webTarget.queryParam("title", title).queryParam("description", description).queryParam("isbn", isbn).queryParam("author", author).queryParam("publisher", publisher).path("book/add").request().post(null, Response.class);
     }
 
