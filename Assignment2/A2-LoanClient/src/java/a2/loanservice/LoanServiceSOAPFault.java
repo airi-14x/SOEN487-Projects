9c323f5376180cd4e1f8281aa722f4dfe2768484
airi-14x <airi.14x@gmail.com>
2020-03-21 11:09:48 -0400

    Duplicate code to test for LoanServiceClient

---
+++ Assignment2/A2-LoanClient/src/java/a2/loanservice/LoanServiceSOAPFault.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a2.loanservice;
+
+import javax.xml.ws.WebFault;
+
+/**
+ *
+ * @author Airi
+ */
+@WebFault(name="LoanServiceSOAPFault")
+public class LoanServiceSOAPFault extends Exception {
+
+    /**
+     * Constructs an instance of <code>SOAPFault</code> with the specified
+     * detail message.
+     *
+     * @param msg the detail message.
+     */
+    public LoanServiceSOAPFault(String msg) {
+        super(msg);
+    }
+    
+    public LoanServiceSOAPFault(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
