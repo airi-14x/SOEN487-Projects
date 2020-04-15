748b7185e0c6f6e0310945546210ee4cb8023588
airi-14x <airi.14x@gmail.com>
2020-04-15 16:47:55 -0400

    Integrate and test formatted functions. Functional

--- Assignment3/UIFunction.py
+++ Assignment3/UIFunction.py
@@ -2,4 +2,13 @@ import temperatureServiceAPI as service
 import temperatureSystemCore as temperature
 
 current_service_instance = service.ServiceAPI()
-current_value = current_service_instance.format_url_default("london")
+current_service_instance.format_url_default("london")
+
+# ---- UI calls ---- #
+# OUTPUT: JSON --> modified_attributes.json (With corresponding modified data)
+# ------------------ #
+
+#current_service_instance.format_temperature_to_celsius("temperature.json")
+#current_service_instance.format_temperature_to_fahrenheit("temperature.json")
+#current_service_instance.format_temperature_to_celsius("modified_attributes.json")
+#current_service_instance.format_time_iso("temperature.json")
