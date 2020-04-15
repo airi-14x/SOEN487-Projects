70db557b8ef0cd2df5bef6fb085fcd6c4cec80d3
airi-14x <airi.14x@gmail.com>
2020-04-15 16:26:35 -0400

    Add another conversion function

--- Assignment3/serviceAPICallTest.py
+++ Assignment3/serviceAPICallTest.py
@@ -9,8 +9,6 @@ def format_temperature_to_celsius(file_name):
     # metric --> Celsius
     with open("modified_attributes.json", "w") as outfile:
 
-        # Fahrenheit --> Celsius
-        # C = 5.0/9.0 * (F - 32)
         if current_units == 'imperial':
             current_temperature_celsius = round(
                 fahrenheit_to_celsius(json_obj['current_temperature']), 1)
@@ -25,7 +23,7 @@ def format_temperature_to_celsius(file_name):
                        'current_feels_like': current_feels_like_celsius,
                        'current_min': current_min_celsius,
                        'current_max': current_max_celsius,
-                       'units': 'imperial'}, outfile, indent=4)
+                       'units': 'metric'}, outfile, indent=4)
 
         elif current_units == 'metric':
             json.dump({'current_temperature': round(json_obj['current_temperature'], 1),
@@ -33,6 +31,38 @@ def format_temperature_to_celsius(file_name):
                        'current_min': round(json_obj['current_min'], 1),
                        'current_max': round(json_obj['current_max'], 1),
                        'units': current_units}, outfile, indent=4)
+
+
+def format_temperature_to_fahrenheit(file_name):
+    json_obj = load_file(file_name)
+    current_units = json_obj['units']
+
+    # imperial --> Fahrenheit
+    # metric --> Celsius
+    with open("modified_attributes.json", "w") as outfile:
+
+        if current_units == 'imperial':
+            json.dump({'current_temperature': round(json_obj['current_temperature'], 1),
+                       'current_feels_like': round(json_obj['current_feels_like'], 1),
+                       'current_min': round(json_obj['current_min'], 1),
+                       'current_max': round(json_obj['current_max'], 1),
+                       'units': current_units}, outfile, indent=4)
+
+        elif current_units == 'metric':
+            current_temperature_fahrenheit = round(
+                celsius_to_fahrenheit(json_obj['current_temperature']), 1)
+            current_feels_like_fahrenheit = round(
+                celsius_to_fahrenheit(json_obj['current_feels_like']), 1)
+            current_min_fahrenheit = round(
+                celsius_to_fahrenheit(json_obj['current_min']), 1)
+            current_max_fahrenheit = round(
+                celsius_to_fahrenheit(json_obj['current_max']), 1)
+
+            json.dump({'current_temperature': current_temperature_fahrenheit,
+                       'current_feels_like': current_feels_like_fahrenheit,
+                       'current_min': current_min_fahrenheit,
+                       'current_max': current_max_fahrenheit,
+                       'units': 'imperial'}, outfile, indent=4)
 
 
 def format_time_iso(file_name):
@@ -46,11 +76,6 @@ def format_time_iso(file_name):
                    'iso': 'true'}, outfile, indent=4)
 
 
-def fahrenheit_to_celsius(value):
-    value = (5.0 / 9.0) * (value - 32)
-    return value
-
-
 def load_file(file_name):
     with open(file_name, 'r') as file:
         json_data = file.read()
@@ -59,5 +84,17 @@ def load_file(file_name):
     return json_obj
 
 
+def fahrenheit_to_celsius(value):
+    value = (5.0 / 9.0) * (value - 32)
+    return value
+
+
+def celsius_to_fahrenheit(value):
+    value = (9.0 / 5.0) * value + 32
+    return value
+
+
 #format_temperature_to_celsius("temperature.json")
-format_time_iso("temperature.json")
+#format_temperature_to_fahrenheit("temperature.json")
+format_temperature_to_celsius("modified_attributes.json")
+# format_time_iso("temperature.json")
